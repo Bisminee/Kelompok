ef8b8b30896f88c6c7bba531c971c07d9cddb23b
ddickyy <dikiidarmawam05x2@gmail.com>
2024-09-04 11:55:16 +0700

    Jobsheet1_DIcky

---
+++ Tugas01.java
@@ -0,0 +1,20 @@
+/**
+ * Tugas01
+ */
+public class Tugas01 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("---------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Nama\t\t: Dicky Darmawan");
+        System.out.println("NIM\t\t: 244107020037");
+        System.out.println("Kelas\t\t: 1B");
+        System.out.println("No Absen\t: 09");
+        System.out.println("Prodi\t\t: Teknik Informatika");
+        System.out.println("Alamat\t\t: Sukorejo, Pasuruan");
+        System.out.println("No HP\t\t: 083846960629");
+        System.out.println("Hobi\t\t: Mendengarkan musik & bernyanyi");
+    }
+}
