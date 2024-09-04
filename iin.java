42608bdbdb2b5822a58804a3409fd634b78b28c5
SitiMutmainah15 <iinsmk1rbg@gmail.com>
2024-09-04 12:13:42 +0700

    IIN

---
+++ iin.java
@@ -0,0 +1,19 @@
+/**
+ * iin
+ */
+public class iin {
+
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.println("--------------------------------------------------------- ");
+        System.out.println("Nama        : Siti Mutmainah");
+        System.out.println("NIM         : 244107020143");
+        System.out.println("Kelas       : 1B");
+        System.out.println("No Absen    : 22");
+        System.out.println("Prodi       : D-IV Teknik Informatika");
+        System.out.println("Alamat      : Desa Karangsekar, Kecamatan kaliori, Kabupaten Rembang");
+        System.out.println("No Hp       : 081818465051");
+        System.out.println("Hobi        : Olahraga");
+    }
+}
